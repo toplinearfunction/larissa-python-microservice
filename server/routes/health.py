af2baa85b807d05dbc38bec50c1cee6ccfa33ab0
toplinearfunction <larissa@if.ufrj.br>
2019-03-20 10:51:51 -0300

    ...

--- server/routes/health.py
+++ server/routes/health.py
@@ -13,7 +13,7 @@ def answer():
     answer = {'The Answer to Life the Universe and Everything': 42}
     return jsonify(answer)
 
-    @app.route('/answer/<number>')
+@app.route('/answer/<number>')
 def CheckAnswerToLifeTheUniverseAndEverything(number):
     content = {
         'statement': 'The Answer to Life the Universe and Everything is ' + number + '.',
